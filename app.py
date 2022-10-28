3c74a6ff1dd283fc6cea13890c8092e078d96eb8
John <eshy008@gmail.com>
2022-10-28 15:26:41 +0100

    fixed cors issue

--- app.py
+++ app.py
@@ -3,15 +3,17 @@
 
 from flask import *
 import json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
-def home_page():
-  data_set = {'slackUsername': '@eshi', 'backend': True, 'age' : 32, 'bio' : 'An enterpreneur, got a lot of experience with Php and taking up a new challenge using Python'}
-  json_dump = json.dumps(data_set)
 
-  return json_dump
+def home_page():
+    data_set = {'slackUsername': '@eshi', 'backend': True, 'age' : 32, 'bio' : 'An enterpreneur, got a lot of experience with Php and taking up a new challenge using Python'}
+    json_dump = json.dumps(data_set)
+    return json_dump
 
 
 
